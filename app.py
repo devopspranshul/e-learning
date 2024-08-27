57d31e6da8e3657d2eebd59d01cd1b7651f972f0
Pranshul Singh <147023403+devopspranshul@users.noreply.github.com>
2024-08-27 15:45:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
